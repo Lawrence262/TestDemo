75bfa557e71892e73a1f5ade4e902dd87d90476d
Lawrence262 <lawrencemanoj99@gmail.com>
2021-06-03 11:34:36 +0530

    Add project files.

---
+++ ClassLibraryTestProject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTestProject
+{
+    public class Class1
+    {
+        public int Add(int i, int j)
+        {  
+            return i + j;
+        }
+    }
+}
